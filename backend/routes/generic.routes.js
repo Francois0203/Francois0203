65b7ef56727719326c99b96aae07009f6ed6935d
Francois Meiring <126506333+Francois0203@users.noreply.github.com>
2025-12-12 14:14:07 +0200

    Clean Repo

--- backend/routes/generic.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const genericController = require("../controllers/generic.controller");
-
-// General List
-router.get("/general-list", genericController.fetchResource("/generic/general-list"));
-
-module.exports = router;
--- backend/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const genericRoutes = require('./generic.routes');
-
-router.use('/generic', genericRoutes)
-
-module.exports = router;
