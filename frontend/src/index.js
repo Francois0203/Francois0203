e83b5da1e4e886f417b3457b875b63009cd36fd5
Francois Meiring <126506333+Francois0203@users.noreply.github.com>
2025-12-12 15:26:45 +0200

    Import UI Styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+/* Styling  */
+import '@Francois0203/ui/dist/styles/Theme.css';
+import '@Francois0203/ui/dist/styles/Components.css';
+import '@Francois0203/ui/dist/styles/GeneralWrappers.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
